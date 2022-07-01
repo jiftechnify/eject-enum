dc1bf55cf344b0ac96e2c8952afa834cf0f2949e
jiftechnify <jiftech.stlfy@gmail.com>
2022-07-01 11:49:12 +0900

    Chore: add test for enums that have const enum expression

---
+++ test/cases/number_const_expr/expected.ts
@@ -0,0 +1,21 @@
+export const ConstExpr = {
+  Add: 3,
+  Sub: 2,
+  Mul: 6,
+  Div: 5,
+  Mod: 4,
+  Shl: 8,
+  Shr: -16,
+  Ushr: 16,
+  And: 4608,
+  Or: 4660,
+  Xor: 9,
+  Minus: -3,
+  Inv: -1,
+  Cat: "abcdef",
+  One: 1,
+  Two: 2,
+  Three: 3,
+} as const;
+
+export type ConstExpr = typeof ConstExpr[keyof typeof ConstExpr];
